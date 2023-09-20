81e23479b38303faa3cbd92968e141f8305791b6
Nick Nyanjui <nicknyanjui@gmail.com>
2023-09-20 13:30:22 +0300

    Add Oauth configuration with Auth0 provider

--- django_openid_auth/django_openid_auth/urls.py
+++ django_openid_auth/django_openid_auth/urls.py
@@ -1,14 +1,26 @@
+import app.oauth as oauth
+from app.views import CustomerOrdersView
+from app.views import CustomersView
+from app.views import OrdersView
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from app.views import CustomersView, OrdersView, CustomerOrdersView
 
 router = DefaultRouter()
-router.register(r'customers', CustomersView, basename='customers')
-router.register(r'orders', OrdersView, basename='orders')
+router.register(r"customers", CustomersView, basename="customers")
+router.register(r"orders", OrdersView, basename="orders")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('customers/<int:customer_id>/orders/', CustomerOrdersView.as_view(), name='customer-orders'),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+    path(
+        "customers/<int:customer_id>/orders/",
+        CustomerOrdersView.as_view(),
+        name="customer-orders",
+    ),
+    path("oauth_home", oauth.oauth_index, name="oauth_index"),
+    path("login", oauth.login, name="login"),
+    path("logout", oauth.logout, name="logout"),
+    path("callback", oauth.callback, name="callback"),
 ]
